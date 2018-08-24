74be10546051ac7f3ccb10a47ea2a5f7eb2d8921
Gurmeet Singh <swiftgurmeet@users.noreply.github.com>
2018-08-23 21:50:15 -0700

    Update ui.R

--- ui.R
+++ ui.R
@@ -10,7 +10,7 @@ shinyUI(pageWithSidebar(
      checkboxInput("famhist", label = "Yes", value = FALSE),
      sliderInput("sbp", "Blood Pressure (Systolic)",value = 110, min = 100, max = 220, step = 1), 
      sliderInput("tobacco", "Tobacco Consumption",value = 0, min = 0, max = 100, step = 1), 
-     sliderInput("ldl", "LDL Value",value = 3, min = 0.5, max = 20, step = 0.25), 
+     sliderInput("ldl", "LDL/HDL Ratio",value = 3, min = 0.5, max = 20, step = 0.25), 
      sliderInput("alcohol", "Alcohol Consumption",value = 0, min = 0, max = 200, step = 5), 
      sliderInput("age", "Pick patient's age",value = 50, min = 20, max = 100, step = 1)
    ) ,
